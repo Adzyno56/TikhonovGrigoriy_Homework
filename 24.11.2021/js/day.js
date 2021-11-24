f27e1b86c57cfb930308b5a593de6955709a190a
Tikhonov Grigoriy <tikhonovg.99@gmail.com>
2021-11-25 00:37:16 +0500

    24.11.2021

---
+++ 24.11.2021/js/day.js
@@ -0,0 +1,9 @@
+let main = document.querySelector("main");
+let htwo = main.querySelector(".names .htwo");
+let about_p = main.querySelector(".about p");
+let call_p = main.querySelector(".call p");
+
+main.style.background = "#E2A117";
+htwo.style.color = "black";
+about_p.style.color = "black";
+call_p.style.color = "black";
